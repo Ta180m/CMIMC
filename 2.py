703c1162a97cbb1e7dc77d558e64fcd68c834a50
Anthony Wang <ta180m@gmail.com>
2021-01-30 11:13:29 -0600

    Add 2.py stuff

--- 2.py
+++ 2.py
@@ -6,7 +6,16 @@ lines = open("in").readlines()
 x = []
 y = []
 for line in lines:
-    x = line.split()[0]
-    y = line.split()[1]
+    x.append(int(line.split()[0]))
+    y.append(int(line.split()[1]))
+
+R = open("r").readlines()
+ans = open("ans").readlines()
+
+for i in range(0, 20):
+    c = plt.Circle((int(ans[i].split()[0]), int(ans[i].split()[1])), radius=int(R[i]))
+    plt.gca().add_artist(c)
 
 plt.scatter(x, y)
+plt.show()
+
